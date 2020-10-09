d115d4f8632a759b9e8e70f8aaf8a257a63d7c2e
Ariel Cardoso <arielvix@gmail.com>
2020-10-09 20:13:19 -0300

    Initial screen

---
+++ assets/js/game.js
@@ -0,0 +1,10 @@
+function startScreen() {
+    $('#start-screen').show();
+    $('#win-screen').hide();
+    $('#overlay').hide();
+    $('#game-screen').hide();
+}
+
+$(document).ready(function(){
+    startScreen();
+})
